fdda6e9de3f66e5b69ed5c4cf8b1c600240ec958
MarvinReinke <marvinreinke123@gmail.com>
2021-10-24 08:31:31 +0200

    init

---
+++ src/main/java/Wochenplaner/wochenplaner/WochenplanerApplication.java
@@ -0,0 +1,13 @@
+package Wochenplaner.wochenplaner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WochenplanerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WochenplanerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Wochenplaner/wochenplaner/WochenplanerApplicationTests.java
@@ -0,0 +1,13 @@
+package Wochenplaner.wochenplaner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WochenplanerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
